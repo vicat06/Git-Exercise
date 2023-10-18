8b2dd5a59d3c7dcb29ce029826016cdc62c26876
vicat06 <vica.treyvus@gmail.com>
2023-10-18 12:19:51 +0300

    Update adcClass.py
    added maxVolt and bitNum to Adc class

--- adcClass.py
+++ adcClass.py
@@ -2,18 +2,24 @@ import re
 
 
 class Adc:
-    def __init__(self) -> None:
+    def __init__(self, maxVolt, bitNum) -> None:
+        self.maxVolt = maxVolt
+        self.bitNum = bitNum
         self.analogVolt = 2.3
+
+    def setAnalogVolt(self, value):
+        if value >= 0 and value <= self.maxVolt:
+            self.analogVolt = value
 
 
 
     def toDigital(self):
         # returns digital number according to analog value
 
-        decimalDigitalNum = (2**10 - 1) / 5 * self.analogVolt       # calculate digital number in decimal
-        decimalDigitalNum = int(round(decimalDigitalNum))           # round digital number to int
-        digitalNum = bin(decimalDigitalNum)                         # convert digital number to binary
-        digitalNum = int(digitalNum[2:])                            # get only the binary value
+        decimalDigitalNum = (2**self.bitNum - 1) / self.maxVolt * self.analogVolt       # calculate digital number in decimal
+        decimalDigitalNum = int(round(decimalDigitalNum))                               # round digital number to int
+        digitalNum = bin(decimalDigitalNum)                                             # convert digital number to binary
+        digitalNum = int(digitalNum[2:])                                                # get only the binary value
         return digitalNum
 
 
@@ -25,10 +31,10 @@ class Adc:
             return
         
         decimalDigitalNum = int(digitalNum, 2)
-        analogVolt = decimalDigitalNum / (2**10 - 1) * 5
-        self.analogVolt = analogVolt
+        analogVolt = decimalDigitalNum / (2**self.bitNum - 1) * self.maxVolt
+        self.setAnalogVolt(analogVolt)
 
     def isBinaryNumCorrect(self, binaryNum):
-        if len(binaryNum) > 10 or re.search(r'[^01]', binaryNum):
+        if len(binaryNum) > self.bitNum or re.search(r'[^01]', binaryNum):
             return False
         return True
